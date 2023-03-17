ad3a3f545a26fb2306e4fc740836b3dec5c7ba1b
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-03-17 10:35:18 -0700

    test build deployment

--- props-lab/src/components/App.jsx
+++ props-lab/src/components/App.jsx
@@ -1,17 +1,42 @@
-import "../styles/App.css";
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Card } from ".";
-
-import DataList from "../data.json";
+// import "../styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      {DataList.map((data, idx) => (
-        <Card {...data} key={idx} />
-      ))}
-    </div>
+    <>
+      <Router>
+        <ul style={{ listStyle: "none", display: "flex", gap: "10px" }}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </Router>
+    </>
   );
+}
+
+function Home() {
+  return <div style={{ color: "orangered" }}>Home</div>;
+}
+
+function About() {
+  return <div style={{ color: "aqua" }}>About</div>;
+}
+
+function Project() {
+  return <div style={{ color: "limegreen" }}>Project</div>;
 }
 
 export default App;
